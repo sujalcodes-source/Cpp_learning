344c759200424ed693b0b391c61774a4bd70c1c4
Sujal Bhagat <sujalbhagatcodes@gmail.com>
2026-01-25 23:39:13 +0545

    Create Goal.cpp
    Code for pratice

---
+++ Practice Code/Goal.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+//Ronaldo is 3 goals less to complete 1000 goals
+using namespace std;
+int main() {
+    int goal;
+    cout<<"Enter your goal"<<endl;
+    cin>>goal;
+    if(goal>=3)
+    {
+        cout<<"Ronaldo complete 1000 goals"<<endl;
+    }
+    else
+    {
+        cout<<"No goal for better luck next time"<<endl;
+    }
+}
