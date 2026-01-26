d74d5df9488ab02b7aad0b142e953b5ca82115af
Sujal Bhagat <sujalbhagatcodes@gmail.com>
2026-01-26 19:38:26 +0545

    TO FIND EVEN NUMBER

---
+++ Practice Code/evennumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter your number"<<endl;
+    cin>>num;
+    if(num%2==0)
+    {
+        cout<<"The number is Even"<<endl;
+    }
+    else
+    {
+        cout<<"The number is odd"<<endl;
+    }
+    return 0;
+}
