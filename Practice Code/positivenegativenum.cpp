ebaad17ef008b3ded7783aadc72c41764d9a08c9
Sujal Bhagat <sujalbhagatcodes@gmail.com>
2026-01-29 21:04:01 +0545

    Create positivenegativenum.cpp

---
+++ Practice Code/positivenegativenum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (n >= 0)
+        cout << "Positive number";
+    else
+        cout << "Negative number";
+
+    return 0;
+}
