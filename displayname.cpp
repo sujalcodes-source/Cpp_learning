f25d349d6886d48a901d960a6271cd929f7dc4ab
Sujal Bhagat <sujalbhagatcodes@gmail.com>
2026-02-04 21:08:12 +0545

    To Display Your Name

---
+++ displayname.cpp
@@ -0,0 +1,11 @@
+// C++ program to demonstrate how to print your
+// own name using cout object
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  
+  	// Printing the name using cout object
+  	cout << "Sujal";
+    return 0;
+}
